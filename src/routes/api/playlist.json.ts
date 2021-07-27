87ffd14ff0d5d480275a83509ea18670e462fba4
snuffyDev <72365477+snuffyDev@users.noreply.github.com>
2021-07-26 21:41:28 -0400

    [feat] adding functionality to longpress action - Abstracting functions from components
    [fix] fixed bug with search
    - pre-fix: refreshing with %20 in url caused full crash/incorrect search

--- src/routes/api/playlist.json.ts
+++ src/routes/api/playlist.json.ts
@@ -10,12 +10,20 @@ import { pb } from '$lib/utils'
  * @param {*} { query }
  * @return {*}  {Promise<EndpointOutput>}
  */
+
 export async function get({ query }): Promise<EndpointOutput> {
-	const browseId = query.get('list')
+	const browseId = query.get('list') || ''
+	const itct = query.get('itct') || ''
+	const ctoken = query.get('ctoken') || ''
 	// console.log(videoId,playlistId)
 	try {
 		const response = await fetch(
-			'https://music.youtube.com/youtubei/v1/browse?key=AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30',
+			'https://music.youtube.com/youtubei/v1/browse?key=AIzaSyC9XL3ZjWddXya6X74dJoCTL-WEYFDNX30' +
+				`${
+					ctoken !== ''
+						? `&ctoken=${ctoken}&continuation=${ctoken}&itct=${itct}&type=next`
+						: ''
+				}`,
 			{
 				method: 'POST',
 				body: JSON.stringify({
@@ -44,42 +52,41 @@ export async function get({ query }): Promise<EndpointOutput> {
 		if (!response.ok) {
 			return { status: response.status, body: response.statusText }
 		}
-		const {
-			header: { musicDetailHeaderRenderer },
-			contents: {
-				singleColumnBrowseResultsRenderer: {
-					tabs: [
-						{
-							tabRenderer: {
-								content: {
-									sectionListRenderer,
-									sectionListRenderer: {
-										contents: [
-											{
-												musicPlaylistShelfRenderer,
-												musicPlaylistShelfRenderer: { contents }
-											}
-										]
+
+		if (ctoken == '') {
+			let {
+				header: { musicDetailHeaderRenderer = {} },
+				contents: {
+					singleColumnBrowseResultsRenderer: {
+						tabs: [
+							{
+								tabRenderer: {
+									content: {
+										sectionListRenderer,
+										sectionListRenderer: {
+											contents: [{ musicPlaylistShelfRenderer = {} }]
+										}
 									}
 								}
 							}
-						}
-					]
+						]
+					}
 				}
-			}
-		} = await response.json()
+			} = await response.json()
 
-		const playlist: {
-			contents: []
-			continuations: unknown
-			playlistId: string
-		} = await sectionListRenderer
-		const playlistId = playlist?.playlistId
+			const {
+				contents: [{ musicPlaylistShelfRenderer: { contents = [] } = {} }] = [],
+				continuations = [],
+				playlistId = ''
+			} = await sectionListRenderer
 
-		const continuations: NextContinuationData = await playlist?.continuations[0]
-			.nextContinuationData
-		const parseHeader = Array(musicDetailHeaderRenderer).map(
-			({ description, subtitle, thumbnail, secondSubtitle, title }) => {
+			const cont: NextContinuationData = await continuations[0]
+				.nextContinuationData
+			musicDetailHeaderRenderer = [musicDetailHeaderRenderer]
+			const parseHeader = musicDetailHeaderRenderer.map((header) => {
+
+				let { description, subtitle, thumbnail, secondSubtitle, title } = header
+				console.log(description, subtitle, thumbnail, secondSubtitle, title)
 				const subtitles: string = pb(subtitle, 'runs:text', false)
 				description = description?.runs[0]?.text
 					? description?.runs[0]?.text
@@ -93,82 +100,50 @@ export async function get({ query }): Promise<EndpointOutput> {
 						thumbnail['croppedSquareThumbnailRenderer']['thumbnail'][
 							'thumbnails'
 						],
-					playlistId,
+					playlistId: musicPlaylistShelfRenderer?.playlistId,
 					secondSubtitle,
 					title: title.runs[0].text
 				}
-			}
-		)[0]
-		// const [contents] = playlist;
-		const parseTrack: Array<PlaylistItem> = contents.map(
-			({ musicResponsiveListItemRenderer }) => {
-				const length = pb(
-					musicResponsiveListItemRenderer,
-					'fixedColumns:0:musicResponsiveListItemFixedColumnRenderer:text:runs:0:text',
-					true
-				)
-				const flexColumns = pb(
-					musicResponsiveListItemRenderer,
-					'flexColumns',
-					true
-				)
+			})[0]
+			// const [contents] = playlist;
 
-				const artistEndpoint = pb(
-					flexColumns,
-					'musicResponsiveListItemFlexColumnRenderer:1:text:runs:0',
-					true
-				)
-				const titleBody = pb(
-					flexColumns,
-					'musicResponsiveListItemFlexColumnRenderer:0:text:runs:0',
-					true
-				)
-				let videoId = undefined
-				if (
-					!musicResponsiveListItemRenderer.playlistItemData &&
-					!musicResponsiveListItemRenderer?.navigationEndpoint?.watchEndpoint
-						?.videoId
-				)
-					return
-				if (musicResponsiveListItemRenderer?.playlistItemData) {
-					videoId = musicResponsiveListItemRenderer?.playlistItemData.videoId
-				} else {
-					videoId = titleBody?.navigationEndpoint?.watchEndpoint?.videoId
-						? titleBody?.navigationEndpoint?.watchEndpoint?.videoId
-						: undefined
-				}
-
-				const title = titleBody.text
-				// console.log(artistEndpoint);
-				const artistInfo: Artist = {
-					artist: artistEndpoint.text,
-					browseId: artistEndpoint?.navigationEndpoint?.browseEndpoint?.browseId
-				}
-				const thumbnail: Thumbnail = pb(
-					musicResponsiveListItemRenderer,
-					'thumbnail:musicThumbnailRenderer:thumbnail:thumbnails',
-					true
-				)
-				return {
-					length,
-					videoId: videoId ? videoId : undefined,
-					playlistId: musicPlaylistShelfRenderer.playlistId,
-					thumbnail,
-					title,
-					artistInfo
-				}
-			}
-		)
-		const Tracks = parseTrack.filter((e) => {
-			return e != null
-		})
-		return {
-			status: 200,
-			body: JSON.stringify({
-				continuations: continuations,
-				tracks: Tracks,
-				header: parseHeader
+			const Tracks = parseTrack(
+				contents,
+				musicPlaylistShelfRenderer?.playlistId
+			).filter((e) => {
+				return e != null
 			})
+			return {
+				status: 200,
+				body: JSON.stringify({
+					continuations: cont,
+					tracks: Tracks,
+					header: parseHeader
+				})
+			}
+		}
+		if (ctoken !== '') {
+			console.log('contin')
+			const {
+				continuationContents: {
+					musicPlaylistShelfContinuation: {
+						contents = [],
+						continuations = []
+					} = {}
+				} = {}
+			} = await response.json()
+			const cont: NextContinuationData = await continuations[0]
+				.nextContinuationData
+			const Tracks = parseTrack(contents.contents).filter((e) => {
+				return e != null
+			})
+			return {
+				status: 200,
+				body: JSON.stringify({
+					continuations: cont,
+					tracks: Tracks
+				})
+			}
 		}
 	} catch (error) {
 		// output to netlify function log
@@ -179,4 +154,62 @@ export async function get({ query }): Promise<EndpointOutput> {
 			body: JSON.stringify({ msg: error.message })
 		}
 	}
+}
+
+function parseTrack(contents = [], playlistId): Array<PlaylistItem> {
+	const Tracks = contents.map((data) => {
+		const { musicResponsiveListItemRenderer } = data
+		const length = pb(
+			musicResponsiveListItemRenderer,
+			'fixedColumns:0:musicResponsiveListItemFixedColumnRenderer:text:runs:0:text',
+			true
+		)
+		const flexColumns = pb(musicResponsiveListItemRenderer, 'flexColumns', true)
+
+		const artistEndpoint = pb(
+			flexColumns,
+			'musicResponsiveListItemFlexColumnRenderer:1:text:runs:0',
+			true
+		)
+		const titleBody = pb(
+			flexColumns,
+			'musicResponsiveListItemFlexColumnRenderer:0:text:runs:0',
+			true
+		)
+		let videoId = undefined
+		if (
+			!musicResponsiveListItemRenderer.playlistItemData &&
+			!musicResponsiveListItemRenderer?.navigationEndpoint?.watchEndpoint
+				?.videoId
+		)
+			return
+		if (musicResponsiveListItemRenderer?.playlistItemData) {
+			videoId = musicResponsiveListItemRenderer?.playlistItemData.videoId
+		} else {
+			videoId = titleBody?.navigationEndpoint?.watchEndpoint?.videoId
+				? titleBody?.navigationEndpoint?.watchEndpoint?.videoId
+				: undefined
+		}
+
+		const title = titleBody.text
+		// console.log(artistEndpoint);
+		const artistInfo: Artist = {
+			artist: artistEndpoint.text,
+			browseId: artistEndpoint?.navigationEndpoint?.browseEndpoint?.browseId
+		}
+		const thumbnail: Thumbnail = pb(
+			musicResponsiveListItemRenderer,
+			'thumbnail:musicThumbnailRenderer:thumbnail:thumbnails',
+			true
+		)
+		return {
+			length,
+			videoId: videoId ? videoId : undefined,
+			playlistId: playlistId,
+			thumbnail,
+			title,
+			artistInfo
+		}
+	})
+	return Tracks
 }
